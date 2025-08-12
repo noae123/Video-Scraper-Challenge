3ae4d6792baa979e38b2a3da7250533ec97c1cca
noae123 <noae123@gmail.com>
2025-08-12 15:08:28 +0300

    handeling mako again

--- part2/fetchHtml.js
+++ part2/fetchHtml.js
@@ -1,6 +1,18 @@
-export async function fetchHtml(url) {
-    const res = await fetch(url);
-    const html = await res.text();
+export async function fetchHtml(url, headers) {
+    let res;
+    if (url.includes("mako")) {
+        res = await fetch(url, {
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Accept-Encoding": "gzip, deflate, br, zstd"
+            }
+        });
+    } else {
+        res = await fetch(url);
+    }
+    const html = await res.text();  
 
     return html;
 }
